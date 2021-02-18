5a636b457fe5f81750024d2dc18c791b79a729d0
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-18 14:49:36 +0700

    Add loading indicator for reader

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -1,12 +1,12 @@
-import { Card, Input } from "antd";
+import { Card, Input, Spin } from "antd";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import DOMPurify from 'dompurify'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faRedo, faPlus, faCog, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faRedo, faPlus, faCog, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faRedo, faPlus, faCog, faSearch)
+library.add(faRedo, faPlus, faCog, faSearch, faSpinner)
 
 function mapStateToProps(state){
   return { reader: state.reader, providers: state.providers }
@@ -16,17 +16,21 @@ const domPurify = DOMPurify(window)
 
 function Reader({ reader, providers, dispatch }){
   const [html, setHtml] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     (async function () {
       // TODO: Add option to disable html sanitization.
+      setIsLoading(true)
+      setHtml("")
       setHtml(domPurify.sanitize(await fetch("http://localhost:1337/cleaner/" + reader.entry.id).then(res => res.text())))
       //setHtml(await fetch("http://localhost:1337/cleaner/" + reader.entry.id).then(res => res.text()))
+      setIsLoading(false)
     })()
   }, [reader.entry])
 
   return (
-    <Card bodyStyle={{display: "flex", flexDirection: "column", padding: 0, minHeight: 0}} style={{flexGrow: 1, display: "flex", flexDirection: "column", minWidth: 0}}>
+    <Card bodyStyle={{display: "flex", flexDirection: "column", padding: 0, minHeight: 0, flexGrow: 1}} style={{flexGrow: 1, display: "flex", flexDirection: "column", minWidth: 0}}>
       <div style={{flexShrink: 1, borderBottom: "1px solid #303030", display: "flex"}}>
         <div style={{display: "flex", flexDirection: "column", flexGrow: 1, minWidth: 0}}>
           <div style={{display: "flex", borderBottom: "1px solid #303030"}}>
@@ -45,7 +49,12 @@ function Reader({ reader, providers, dispatch }){
           </div>
         </div>
       </div>
-      <div style={{overflow: "auto", flexGrow: 1}}><div class="cleanrss-reader" style={{height: '100%'}} dangerouslySetInnerHTML={{__html: html}}></div></div>
+
+      <div style={{overflow: "auto", flexGrow: 1, display: 'flex', flexDirection: "column", justifyContent: "center"}}>
+        <div style={{margin: "auto", display: isLoading? "":"none", fontSize: "5em"}}><FontAwesomeIcon className="fa-spin" icon={["fas", "spinner"]}></FontAwesomeIcon></div>
+        <div class="cleanrss-reader" style={{height: '100%', flexGrow: 1, display: !isLoading? "":"none"}} dangerouslySetInnerHTML={{__html: html}}></div>
+      </div>
+
     </Card>
   )
 }
