93ef629307d381a9566038ad9c7ec74aa358249f
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-18 13:56:31 +0700

    Add blue outline to the active entry card

--- src/components/Browser/EntryCard.js
+++ src/components/Browser/EntryCard.js
@@ -1,8 +1,13 @@
 import { Card } from "antd";
+import { connect } from "react-redux";
 
-export default function EntryCard({ item, onClick }){
+function mapStringToProps(state){
+  return { currentActiveEntryId: state.reader.entry.id }
+}
+
+function EntryCard({ currentActiveEntryId, item, onClick }){
   return (
-    <Card onClick={() => onClick()} hoverable bodyStyle={{paddingTop: 5, paddingBottom: 5}} size="small" title={<div><div style={{whiteSpace: "break-spaces"}}>{item.title}</div></div>}>
+    <Card onClick={() => onClick()} hoverable style={{borderColor: item.id == currentActiveEntryId? "#1890FF":null, boxShadow: item.id == currentActiveEntryId? "0px 0px 5px 2.5px rgba(24,144,255,0.5)":null}} bodyStyle={{paddingTop: 5, paddingBottom: 5}} size="small" title={<div><div style={{whiteSpace: "break-spaces"}}>{item.title}</div></div>}>
       <div style={{display: 'flex'}}>
         <div style={{flexGrow: 1, fontSize: "smaller"}}>{item.author}</div>
         <div style={{textAlign: "right", fontSize: "smaller"}}>{(new Date(item.publishedAt*1000)).toLocaleString("en-GB")}</div>
@@ -10,3 +15,5 @@ export default function EntryCard({ item, onClick }){
     </Card>
   )
 }
+
+export default connect(mapStringToProps, null)(EntryCard)
