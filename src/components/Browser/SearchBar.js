c5594f54697ac17a9181ad35e61c04d3a2e3a0a1
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-18 11:50:55 +0700

    Fix small imperfections on the search bar

--- src/components/Browser/SearchBar.js
+++ src/components/Browser/SearchBar.js
@@ -2,9 +2,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Card, DatePicker, Input } from "antd";
 
 export default function SearchBar(){
-  return <Card size="small" bodyStyle={{padding: 0, display: 'flex', alignItems: "center"}}>
-    <FontAwesomeIcon icon={["fas", "search"]} style={{marginLeft: 6}}/>
-    <Input style={{borderRight: "1px solid #303030"}} bordered={false} placeholder="Search..."></Input>
-    <DatePicker.RangePicker bordered={false}></DatePicker.RangePicker>
+  return <Card size="small" bodyStyle={{padding: 0, display: 'flex', alignItems: "stretch"}}>
+    <div style={{flexGrow: 1, borderRight: "1px solid #303030", display: 'flex', alignItems: "center"}}>
+      <FontAwesomeIcon icon={["fas", "search"]} style={{marginLeft: 6}}/>
+      <Input bordered={false} placeholder="Search..."></Input>
+    </div>
+    <DatePicker.RangePicker bordered={false} showTime></DatePicker.RangePicker>
   </Card>
 }
