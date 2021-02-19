05e0ee447f78c1aa94493e95b74de84659f466f0
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-19 10:09:57 +0700

    Add version info popup

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -1,4 +1,4 @@
-import { Card, Tag, Button } from "antd";
+import { Card, Tag, Button, Popover } from "antd";
 import { useState } from "react";
 import SettingsModal from "./Settings/SettingsModal";
 
@@ -6,8 +6,10 @@ export default function TitleBar(){
   const [isSettingsModalOpen, setIsSettingsModalOpen] = useState(false)
   return (
     <Card bodyStyle={{paddingTop: 5, paddingBottom: 5, display: "flex", alignItems: "baseline"}}>
-      <h4 style={{textAlign: "center", margin: 0}}>CleanRSS</h4>
-      <Tag style={{marginLeft: 5}}>v0.1.0</Tag>
+      <h4 style={{textAlign: "center", margin: 0, userSelect: "none", cursor: "default"}}>CleanRSS</h4>
+      <Popover content="You're on the latest version!" trigger="click">
+        <Tag style={{marginLeft: 5, userSelect: "none", cursor: "pointer"}}>v0.1.0</Tag>
+      </Popover>
       <Button size="small" type="text" onClick={() => setIsSettingsModalOpen(true)}>Settings</Button>
       <SettingsModal isOpen={isSettingsModalOpen} onClose={() => setIsSettingsModalOpen(false)}></SettingsModal>
     </Card>
