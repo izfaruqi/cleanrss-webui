d88066c494d65c3e177c0a696af3e808bd9e378a
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-20 22:59:48 +0700

    Forgot to remove dependencies to the old add provider modal

--- src/components/Browser/Browser.js
+++ src/components/Browser/Browser.js
@@ -2,12 +2,10 @@ import { Button, Row, Col, Layout, Card } from "antd";
 import EntryList from "./EntryList";
 import ProviderTree from "./ProviderTree";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import AddProviderModal from "./AddProviderModal";
 import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 
 export default function Browser({ style }) {
-  const [isAddProviderModalOpen, setIsAddProviderModalOpen] = useState(false)
   return (
     <div style={{display: "flex", gap: 5, alignItems: "stretch", ...style}}>
       <Card style={{flexGrow: 1}} title={
