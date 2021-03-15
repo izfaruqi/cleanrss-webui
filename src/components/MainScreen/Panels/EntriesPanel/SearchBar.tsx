cb26c19f09d7e402bc74676e5cf0ca14509bcd5d
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-15 10:49:13 +0700

    Add search bar to entry list

--- src/components/MainScreen/Panels/EntriesPanel/SearchBar.tsx
+++ src/components/MainScreen/Panels/EntriesPanel/SearchBar.tsx
@@ -1,5 +1,10 @@
+import { DatePicker, Input } from "antd";
+import VBar from "../../../utils/dividers/VBar";
+
 export default function SearchBar(){
-  return <div>
-    Search bar
+  return <div style={{ display: 'flex' }}>
+    <Input style={{ flexGrow: 1 }} bordered={false} />
+    <VBar />
+    <DatePicker.RangePicker bordered={false} />
   </div>
 }
