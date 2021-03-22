148545a1cb306d56db8b30262c088151bbb4488a
Ibrahim Faruqi <izf@izfaruqi.com>
2021-03-22 07:39:54 +0700

    Add settings modal skeleton

--- src/components/MainScreen/TopBar.tsx
+++ src/components/MainScreen/TopBar.tsx
@@ -1,10 +1,12 @@
 import { Tag } from "antd"
 import StatusIndicator from "./StatusIndicator"
+import TopBarMenu from "./TopBarMenu"
 
 export default function TopBar(){
   return <div style={{ padding: "5px 10px", display: 'flex', gap: 10, alignItems: 'baseline' }}>
     <span>CleanRSS</span>
     <Tag>v0.1.0</Tag>
+    <TopBarMenu></TopBarMenu>
     <div style={{ flexGrow: 1 }}></div>
     <StatusIndicator />
   </div>
--- src/fa.ts
+++ src/fa.ts
@@ -1,4 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faExternalLinkAlt, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faExternalLinkAlt, faSpinner, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faSpinner, faExternalLinkAlt)
+library.add(faSpinner, faExternalLinkAlt, faAngleDown)
